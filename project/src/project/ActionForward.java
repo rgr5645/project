63d58f388eeb630aef5aac259df4628b160aa1d8
kim <minyjoy@hotmail.com>
2019-03-27 11:10:44 +0900

    ActionForward 업로드

---
+++ project/src/project/ActionForward.java
@@ -0,0 +1,36 @@
+package project;
+
+public class ActionForward {
+	private String path; 
+	private boolean redirect; 
+		
+	public ActionForward() {
+		super();
+	}
+
+	public ActionForward(String path, boolean redirect) {
+		super();
+		this.path = path;
+		this.redirect = redirect;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public boolean isRedirect() {
+		return redirect;
+	}
+
+	public void setRedirect(boolean redirect) {
+		this.redirect = redirect;
+	}
+	
+	
+}
+
+
