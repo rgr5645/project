e1e044ba14785238d7b7d281439b7bd624c8a6d8
kang <rgr5645@naver.com>
2019-03-27 11:10:40 +0900

    MemberDAO 추가

---
+++ project/src/project/MemberDAO.java
@@ -0,0 +1,22 @@
+package project;
+
+import java.sql.Connection;
+
+public class MemberDAO {
+	private Connection con;
+	
+	
+	
+	public MemberDAO(Connection con) {
+		super();
+		this.con = con;
+	}
+
+	public void insert() {
+		
+	}
+	
+	public void modify() {
+		
+	}
+}
