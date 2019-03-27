b3fcd1cec6c69b1741bda88c8a264952dccf0172
oracle <kkms1205@naver.com>
2019-03-27 11:17:23 +0900

    index 값 1 추가

--- project/src/project/Index.java
+++ project/src/project/Index.java
@@ -2,6 +2,9 @@ package project;
 
 public class Index {
 	public static void main(String[] args) {
-		
+		System.out.println("HTML");
+		System.out.println("CSS");
+		System.out.println("JAVASCRIPT");
+		System.out.println("JQUERY");
 	}
 }
